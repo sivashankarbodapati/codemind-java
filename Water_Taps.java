f9592b731cfcf6f75c69cdc7f1bd086cc19106d7
sivashankarbodapati <119917360+sivashankarbodapati@users.noreply.github.com>
2024-01-08 19:23:47 +0530

    import java.util.Scanner; public class WaterTaps{     public static void main(String[]ards){         Scanner sc = new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         System.out.println((a*b)/(a+b));     } }

---
+++ Water_Taps.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class WaterTaps{
+    public static void main(String[]ards){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println((a*b)/(a+b));
+    }
+}
